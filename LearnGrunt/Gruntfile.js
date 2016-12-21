5ec9790777175259563c234255a7f643def9b654
Deven Shah <deven.shah@capita.co.uk>
2016-12-21 12:47:14 +0000

    Added JSHint Task to Grunt

--- LearnGrunt/Gruntfile.js
+++ LearnGrunt/Gruntfile.js
@@ -5,9 +5,19 @@ module.exports = function(grunt){
     pkg: grunt.file.readJSON('package.json'),
     clean: {
       output: ['ToBeCleaned/*']
+    },
+    jshint:{
+      options:{
+        //force: true  ---ignores all the errors
+        //'-W069' : false ---ignores particular error
+        ignores: ['./app/scripts/3rdparty/*.js'],// ---ignores files in the list
+        reporterOutput: 'jshint.log'
+      },
+      files:['app/scripts/*/*.js']
     }
   });
 
   grunt.loadNpmTasks("grunt-contrib-clean");
-  grunt.registerTask("default", ["clean"]);
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.registerTask("default", ["clean","jshint"]);
 }
---
+++ LearnGrunt/app/scripts/domain/app.js
@@ -0,0 +1,9 @@
+(function(){
+  'use strict';
+  module.exports = function(){
+    var sayHello = function(){
+      alert("HELLO WORLD!");
+    };
+    sayHello();
+  };
+}());
