237f934e61eb1f8582294c0c0b945b6c7afcb65b
Deven Shah <deven.shah@capita.co.uk>
2016-12-21 10:41:41 +0000

    setup grunt and first task within

---
+++ LearnGrunt/Gruntfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = function(grunt){
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      output: ['ToBeCleaned/*']
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.registerTask("default", ["clean"]);
+}
---
+++ LearnGrunt/README.md
@@ -0,0 +1,11 @@
+Install Node
+
+npm install -g grunt-cli
+
+create folder and run following command to install grunt in that folder
+- npm install grunt --save-dev
+
+install first plugin
+- npm install grunt-contrib-clean --save-dev
+
+Create Gruntfile.js
